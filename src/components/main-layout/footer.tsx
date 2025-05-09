d256edd3b62a1716a53d70139689ef34902e50ac
bimangunarathne <bimangunarathne@outlook.com>
2025-05-09 11:55:07 +0530

    feat: update footer styling for improved visibility and layout consistency

--- src/components/main-layout/footer.tsx
+++ src/components/main-layout/footer.tsx
@@ -109,7 +109,7 @@ function DesktopFooter() {
         </div>
       </div>
 
-      <div className="border-t border-gray-800 mt-6 py-3 px-7 flex justify-between items-center">
+      <div className="border-t border-[#232732] mt-6 py-3 px-7 flex justify-between items-center">
         <div className="flex">
           <p className="text-sm text-[#DBDBDB]">© All rights reserved by AgroWorld Pvt Ltd</p>
           <div className="flex ml-5">
@@ -131,7 +131,7 @@ function MobileFooter() {
   return (
     <div className="mx-auto">
       <div className="mb-6 p-5 flex flex-col items-center">
-        <h2 className="text-xl text-center font-bold mb-7">MyFarm</h2>
+        <h2 className="text-xl text-center font-bold mb-10 mt-5">MyFarm</h2>
         <div className="px-10">
           <div className="flex items-start mb-8">
             <div className="text-[#8492A3] mr-2 mt-1">
@@ -171,25 +171,25 @@ function MobileFooter() {
 
       <div className="flex justify-between mb-6 px-16">
         <div>
-          <h3 className="text-lg font-semibold mb-3">Quick Links</h3>
+          <h3 className="text-lg font-semibold mb-5 text-[#FFFFFF]">Quick Links</h3>
           <ul>
-            <li className="mb-2"><a href="#" className="hover:text-blue-300">Home</a></li>
-            <li className="mb-2"><a href="#" className="hover:text-blue-300">Privacy Policy</a></li>
-            <li className="mb-2"><a href="#" className="hover:text-blue-300">Terms & Conditions</a></li>
+            <li className="mb-3"><a href="#" className="text-[#DBDBDB] hover:text-blue-300">Home</a></li>
+            <li className="mb-3"><a href="#" className="text-[#DBDBDB] hover:text-blue-300">Privacy Policy</a></li>
+            <li className="mb-3"><a href="#" className="text-[#DBDBDB] hover:text-blue-300">Terms & Conditions</a></li>
           </ul>
         </div>
 
         <div>
-          <h3 className="text-lg font-semibold mb-3">My Accounts</h3>
+          <h3 className="text-lg font-semibold mb-5 text-[#FFFFFF]">My Accounts</h3>
           <ul>
-            <li className="mb-2"><a href="#" className="hover:text-blue-300">My Account</a></li>
-            <li className="mb-2"><a href="#" className="hover:text-blue-300">My Cart</a></li>
-            <li className="mb-2"><a href="#" className="hover:text-blue-300">My Order History</a></li>
+            <li className="mb-3"><a href="#" className="text-[#DBDBDB] hover:text-blue-300">My Account</a></li>
+            <li className="mb-3"><a href="#" className="text-[#DBDBDB] hover:text-blue-300">My Cart</a></li>
+            <li className="mb-3"><a href="#" className="text-[#DBDBDB] hover:text-blue-300">My Order History</a></li>
           </ul>
         </div>
       </div>
 
-      <div className="flex justify-center space-x-6 mb-6">
+      <div className="flex justify-center space-x-6 mt-20 mb-20">
         <a href="#" className="text-blue-400 hover:text-blue-300">
           <Image src={Linkedin} alt="LinkedIn" className='w-auto h-8 object-cover' />
         </a>
@@ -207,15 +207,18 @@ function MobileFooter() {
         </a>
       </div>
 
-      <div className="border-t border-gray-800 pt-4 text-center">
-        <p className="text-xs text-gray-400 mb-3">© All rights reserved by AgroWorld Pvt Ltd</p>
-        <div className="flex justify-center mb-3">
-          <Image src={Visa} alt="Visa" className='w-auto h-6 object-cover mr-2' />
-          <Image src={MasterCard} alt="MasterCard" className='w-auto h-6 object-cover' />
+      <div className="border-t border-[#404451] p-6 text-center">
+        <div className="flex justify-between items-center">
+          <p className="text-xs text-[#DBDBDB] mb-3">© All rights reserved by AgroWorld Pvt Ltd</p>
+          <div className="flex justify-center mb-3">
+            <Image src={Visa} alt="Visa" className='w-auto h-6 object-cover mr-2' />
+            <Image src={MasterCard} alt="MasterCard" className='w-auto h-6 object-cover' />
+          </div>
         </div>
-        <div className="flex justify-center space-x-4 text-xs">
-          <a href="#" className="text-gray-400 hover:text-white">Terms & Conditions</a>
-          <a href="#" className="text-gray-400 hover:text-white">Privacy Policy</a>
+
+        <div className="flex justify-between space-x-4 text-xs">
+          <a href="#" className="text-[#DBDBDB] hover:text-white underline">Terms & Conditions</a>
+          <a href="#" className="text-[#DBDBDB] hover:text-white underline">Privacy Policy</a>
         </div>
       </div>
     </div>
