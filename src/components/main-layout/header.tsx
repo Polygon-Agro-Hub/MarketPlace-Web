a26a275ad6d68a2d830b4fff39cb3103c6a3f372
Indeepa-Biman-Gunarathne <58646947+Indeepa-Biman-Gunarathne@users.noreply.github.com>
2025-05-23 13:55:52 +0530

    remove user first name from contact information in header

--- src/components/main-layout/header.tsx
+++ src/components/main-layout/header.tsx
@@ -51,7 +51,7 @@ const Header = () => {
         <div className='bg-[#2C2C2C] text-gray-300 py-2 px-7'>
           <div className="mx-auto flex justify-between items-center">
             <span className="text-sm italic">
-              Call us for any query or help +94 770 111 999 {user ? user.firstName : ''}
+              Call us for any query or help +94 770 111 999
             </span>
             <div className="flex gap-2">
               <Link href="/signup" className="text-sm bg-gray-700 rounded-full px-4 py-1 hover:bg-gray-600">
