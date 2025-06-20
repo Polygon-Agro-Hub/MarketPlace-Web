d0cc36495c57426d3632900fe161c1805b47af1a
Pasan-FDo <pasanfernando.work@gmail.com>
2025-06-20 12:21:51 +0530

    fix: Remove unnecessary client directive and environment variable logging from Error404 page

--- src/app/error/404/page.tsx
+++ src/app/error/404/page.tsx
@@ -1,18 +1,11 @@
-'use client';
 import Image from 'next/image';
-import { useEffect } from 'react';
 
-useEffect(()=>{
-  console.log("NEXT_PUBLIC_BASE_PATH:",process.env.NEXT_PUBLIC_BASE_PATH );
-  console.log("NODE_ENV:",process.env.NODE_ENV );
-  console.log("NEXT_PUBLIC_API_BASE_URL:",process.env.NEXT_PUBLIC_API_BASE_URL );
-  
-})
+
 export default function Error404() {
   return (
     <div className="flex flex-col items-center justify-start min-h-screen pt-10 bg-white px-4 text-center">
       <Image
-       src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/404.jpg`}
+        src="/404.jpg"
         alt="Page Not Found"
         width={400}
         height={400}
