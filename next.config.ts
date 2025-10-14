6d1c7cc809f34fb8319433a303376d2e1fa9c16b
Tharaka Nuwan <tnathuluwage@gmail.com>
2025-10-14 12:28:58 +0530

    image loading cloudfare issue

--- next.config.ts
+++ next.config.ts
@@ -22,19 +22,20 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   basePath: process.env.NODE_ENV == "development" ? '' : process.env.NEXT_PUBLIC_BASE_PATH ?? '/market',
-  assetPrefix: process.env.NODE_ENV == "development" ? '' : process.env.NEXT_PUBLIC_BASE_PATH ??  '/market',
+  assetPrefix: process.env.NODE_ENV == "development" ? '' : process.env.NEXT_PUBLIC_BASE_PATH ?? '/market',
   trailingSlash: true,
 
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "pub-79ee03a4a23e4dbbb70c7d799d3cb786.r2.dev",
-        port: "",
-        pathname: "/**",
+        hostname: "**",
       },
     ],
   },
 };
 
 export default nextConfig;
+
+//production hostname - hostname: "pub-a5aee6f14995472dbf4fdc3013cf95e4.r2.dev",
+//development hostname - hostname: "pub-79ee03a4a23e4dbbb70c7d799d3cb786.r2.dev",
